8fddcd363f2966cb9f51b094d7a7e0cc7ccfdb1d
Andres Turullols <aturullols221@student.fuhsd.org>
2026-09-02 22:02:10 -0500

    Added more comments

--- Building Blocks/Modules/volumescaling.h
+++ Building Blocks/Modules/volumescaling.h
@@ -8,7 +8,7 @@
 const float MAX_SAMPLE_VALUE = 2147483648.0f; // 2^31
 
 const float HP_ALPHA = 0.995f; // closer to 1.0 = lower cutoff frequency
-const float EMA_ALPHA = 0.5f; // for exponentional moving average for dB
+const float EMA_ALPHA = 0.6f; // for exponentional moving average for dB, 1.0 is fastest 0.0 is slowest
 const float NOISE_FLOOR_DB = -40.0f; // the softest input sound before silent output
 const float NOISE_CEILING_DB  = -6.0f; // the loudest input sound before maxing out ouput
 
